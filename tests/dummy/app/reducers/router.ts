0ae2355b848c2582f2a50c8aff32637a5c111889
Thomas Chen <tom.chen@am.sony.com>
2018-05-22 15:47:29 -0700

    remove unused code

--- tests/dummy/app/reducers/router.ts
+++ tests/dummy/app/reducers/router.ts
@@ -6,24 +6,11 @@ export type RouterState = RoutePart[];
 
 const INITIAL_STATE: RouterState = [RoutePart.app, RoutePart.home, RoutePart.index];
 
-interface ActivateAction extends Action {
-  path: RoutePart
-}
-
 interface SetAction extends Action {
   paths: RoutePart[]
 }
 
 export default createReducer<RouterState>(INITIAL_STATE, {
-  [ActionName.RESET_ROUTE](state: RouterState, action: Action): RouterState {
-    return [];
-  },
-  [ActionName.PUSH_ROUTE](state: RouterState, action: ActivateAction): RouterState {
-    return state.concat(action.path);
-  },
-  [ActionName.POP_ROUTE](state: RouterState, action: Action): RouterState {
-    return state.slice(0, -1);
-  },
   [ActionName.SET_ROUTE](state: RouterState, action: SetAction): RouterState {
     return action.paths;
   }
