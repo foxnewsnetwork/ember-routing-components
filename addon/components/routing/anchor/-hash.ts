5419afc51675956b166224f82f4874c3861d5eb6
Thomas Chen <tom.chen@am.sony.com>
2018-05-22 17:02:02 -0700

    start out on ember's hash routing type (others are location, none, and auto)

---
+++ addon/components/routing/anchor/-hash.ts
@@ -0,0 +1,10 @@
+import Component from '@ember/component';
+// @ts-ignore: Ignore import of compiled template
+import layout from '../../../templates/components/routing/anchor/-hash';
+
+export default class RoutingAnchorHash extends Component.extend({
+  tagName: ''
+}) {
+  layout = layout;
+  // normal class body definition here
+};
