166d1a28f431a6ac97bdfa5cf8c53315898d799c
Thomas Chen <tom.chen@am.sony.com>
2018-06-01 09:33:09 -0700

    wip in progress for making the ember anchor hash

--- addon/components/routing/anchor/-hash.ts
+++ addon/components/routing/anchor/-hash.ts
@@ -1,10 +1,37 @@
 import Component from '@ember/component';
 // @ts-ignore: Ignore import of compiled template
 import layout from '../../../templates/components/routing/anchor/-hash';
+import { set, computed } from '@ember/object';
 
-export default class RoutingAnchorHash extends Component.extend({
+interface RouteParser<P> {
+  (url: string): [string, P]
+}
+
+export default class RoutingAnchorHash<P> extends Component.extend({
   tagName: ''
 }) {
   layout = layout;
   // normal class body definition here
+
+  url: string;
+
+  routePaths = computed('url', {
+    get(this: RoutingAnchorHash<P>) {
+
+    }
+  }).readOnly();
+
+  didInsertElement(this: RoutingAnchorHash) {
+    set(this, 'newURL', window.location.hash);
+    window.addEventListener('hashchange', this.onHashChange);
+  }
+
+  willDestroyElement(this: RoutingAnchorHash) {
+    window.removeEventListener('hashchange', this.onHashChange);
+  }
+
+  onHashChange = (event: HashChangeEvent) => {
+    set(this, 'newURL', event.newURL);
+    set(this, 'oldURL', event.oldURL);
+  }
 };
